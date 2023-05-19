7e600d849a26f02c5c47264e6075d4bc05bd7782
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-19 14:02:58 -0500

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/dogs" => "dogs#index"
+  get "/dogs/:id" => "dogs#show"
   post "/dogs" => "dogs#create"
-  # patch "/dogs/:id" => "dogs#update"
-  # delete "/dogs/:id" => "dogs#destroy"
+  patch "/dogs/:id" => "dogs#update"
+  delete "/dogs/:id" => "dogs#destroy"
+
   post "/users" => "users#create"
+  # post "/sessions" => "sessions#create"
 end
